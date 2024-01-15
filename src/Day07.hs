5b4a8366d9d41b231fa477b0193fc0a591d51d1b
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-01-15 14:26:02 +0400

    Day07: star 2

--- src/Day07.hs
+++ src/Day07.hs
@@ -5,6 +5,8 @@ import Control.Applicative
 import Text.Megaparsec (oneOf)
 import qualified Data.Map.Strict as Map
 import Data.List (sortOn)
+import Data.Maybe
+import Debug.Trace
 
 fileContent = parseContent $(getFile)
 
@@ -23,19 +25,20 @@ parseHand = some (oneOf ("AKQJT9876543210" :: [Char]))
 
 -- * Generics
 rankHand hand = do
-  let h = sort $ Map.elems $ Map.fromListWith (+) $ do
+  let h = Map.fromListWith (+) $ do
         c <- hand
         pure (c, 1)
+  (getRank h, map rankLetters hand)
 
-  let rank = case h of
+getRank h = - case sort (Map.elems h) of
         [5] -> 0
-        [_, 4] -> 1
+        [1, 4] -> 1
         [2, 3] -> 2
         [1, 1, 3] -> 3
         [1, 2, 2] -> 4
         [1, 1, 1, 2] -> 5
         [1, 1, 1, 1, 1] -> 6
-  (-rank, map rankLetters hand)
+        e -> error $ "getRank: " <> show e
 
 rankLetters '1' = 1
 rankLetters '2' = 2
@@ -57,7 +60,47 @@ rankLetters 'A' = 14
 day handsAndBids = sum $ zipWith (*) [1..] (map snd $ sortOn (\(hand, _bid) -> rankHand hand) handsAndBids)
 
 -- * SECOND problem
-day' = undefined
+rankHand' hand = do
+  let (traceShowId -> m') = Map.fromListWith (+) $ do
+        c <- hand
+        pure (c, 1)
+  let j = fromMaybe 0 $ Map.lookup 'J' m'
+      m = Map.delete 'J' m'
+  let rank
+        | j == 5 = 0
+        | j == 0 = getRank m
+        | otherwise = maximum $ do
+         (traceShowId -> m') <- newHandFrom j (Map.keys m)
+         pure $ getRank (Map.unionWith (+) m (Map.fromListWith (+) m'))
+          
+  (rank, map rankLetters' hand)
+
+newHandFrom 0 keys = [[]]
+newHandFrom n keys = do
+  k <- keys
+  l' <- ((k, 1):) <$> newHandFrom (n-1) keys
+  pure $ l'
+
+rankLetters' '1' = 1
+rankLetters' '2' = 2
+rankLetters' '3' = 3
+rankLetters' '4' = 4
+rankLetters' '5' = 5
+rankLetters' '6' = 6
+rankLetters' '7' = 7
+rankLetters' '8' = 8
+rankLetters' '9' = 9
+rankLetters' 'T' = 10
+rankLetters' 'J' = 0
+rankLetters' 'Q' = 12
+rankLetters' 'K' = 13
+rankLetters' 'A' = 14
+
+
+-- * FIRST problem
+day' handsAndBids = sum $ zipWith (*) [1..] (map snd $ sortOn (\(hand, _bid) -> rankHand' hand) handsAndBids)
+
+
 
 ex = parseContent [str|32T3K 765
 T55J5 684
