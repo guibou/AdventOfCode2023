0140c30af73536d99d91746808b12d359f81945c
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-01-15 13:22:38 +0400

    Day06: first star

---
+++ src/Day06.hs
@@ -0,0 +1,32 @@
+module Day06 where
+
+import Utils
+import Control.Applicative
+
+fileContent = parseContent $(getFile)
+
+parseContent = unsafeParse $ do
+  symbol "Time:"
+  times <- some parseNumber
+  "\n"
+  symbol "Distance:"
+  distances <- some parseNumber
+  "\n"
+  pure (zip times distances)
+
+-- * Generics
+raceDistance time = map (\t -> (t, t * (time - t))) [0..time]
+
+scoreRace (time, record) = length $ filter (\(t, d) -> d > record) (raceDistance time)
+
+-- * FIRST problem
+day races = product $ map scoreRace races
+
+-- * SECOND problem
+day' = undefined
+
+ex = parseContent [str|Time:      7  15   30
+Distance:  9  40  200
+|]
+
+-- started at Mon Jan 15 01:13:41 PM +04 2024
